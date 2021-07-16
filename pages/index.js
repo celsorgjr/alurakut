7300f1d184369c1804f0045753df94e30b60fa19
Celso Rodrigues Gonçalves <celsorgjr@gmail.com>
2021-07-16 16:18:35 -0300

    Aula 2

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,34 @@
+import React from 'react';
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons'
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/components/lib/AlurakutCommons'
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSideBar(propriedades){
   return (
-    <Box>
+    <Box as="aside">
       <img src={`https://github.com/${propriedades.user}.png`} style={{ borderRadius: '8px' }} />
+      <hr/>
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.user}`}>
+          @{propriedades.user}
+        </a>
+      </p>      
+      <hr/>
+
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
 
 export default function Home() {
-  const githubUser = 'celsorgjr'
+  const githubUser = 'celsorgjr';
+  const [comunidades, setComunidades] = React.useState([{ 
+    id: new Date().toISOString(),
+    title: 'Eu odeio acordar cedo', 
+    image: 'https://alurakut.vercel.app/capa-comunidade-01.jpg' 
+  }]);
+  
   const pessoasFavoritas = [
       'lluccia', 
       'peas', 
@@ -37,8 +53,60 @@ export default function Home() {
 
           <OrkutNostalgicIconSet mensagens="1200" fas="500" fotos="10" recados="12" />
         </Box>
+
+        <Box>
+          <h2 className="subTitle">O que voce deseja fazer?</h2>
+          <form onSubmit={function handleCriaComunidade(e){
+              e.preventDefault();
+              const dadosDoForm = new FormData(e.target);
+
+              const comunidade = {
+                id: new Date().toISOString(),
+                title: dadosDoForm.get('title'),
+                image: dadosDoForm.get('image')
+              };
+
+              // comunidades.push('Alura Stars');
+              setComunidades([...comunidades, comunidade])
+              console.log(comunidades);
+          }}>
+            <div>
+              <input placeHolder="Qual vai ser o nome da sua comunidade?" 
+                name="title"
+                aria-level="Qual vai ser o nome da sua comunidade?"
+              />
+            </div>
+            <div>
+              <input placeHolder="Coloque uma URL para usarmos de capa" 
+                name="image"
+                aria-level="Coloque uma URL para usarmos de capa"
+              />
+            </div>
+
+            <button>
+              Criar comunidade
+            </button>
+          </form>
+        </Box>
       </div>
       <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle">
+            comunidades ({comunidades.length})
+          </h2>
+          <ul>
+            {comunidades.map((pessoa) => {
+              return (
+                <li key={pessoa.id}>
+                  <a href={`/users/${pessoa.title}`} >
+                    <img src={pessoa.image} />
+                    <span>{pessoa.title}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
         <ProfileRelationsBoxWrapper>
           <h2 className="smallTitle">
             Pessoas da comunidades ({pessoasFavoritas.length})
@@ -46,8 +114,8 @@ export default function Home() {
           <ul>
             {pessoasFavoritas.map((pessoa) => {
               return (
-                <li>
-                  <a href={`/users/${pessoa}`} key={pessoa}>
+                <li key={pessoa} >
+                  <a href={`/users/${pessoa}`} >
                     <img src={`https://github.com/${pessoa}.png`} />
                     <span>{pessoa}</span>
                   </a>
@@ -55,12 +123,9 @@ export default function Home() {
               )
             })}
           </ul>
-        </ProfileRelationsBoxWrapper >
-        <Box>
-          Comunidades
-        </Box>
+        </ProfileRelationsBoxWrapper>
       </div>
     </MainGrid>
     </>
   )
-}
+};
